2f8f0324498a523a360533a3d02eeeb18610eff1
Branyer <branyervergarac@gmail.com>
2021-01-26 23:48:02 -0400

    adding header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'user': "url('images/icon.user.png')"
+      }
+    },
   },
   variants: {
     extend: {},
